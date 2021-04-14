57447c90fa6da1e1a7539b80bf44e3e0e6190bba
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2021-04-14 12:56:49 +0700

    lastest

--- server/src/config/routes.js
+++ server/src/config/routes.js
@@ -3,8 +3,6 @@ const router = require('express').Router();
 
 // Import Router
 const auth = require('../routes/auth/auth.router');
-const room = require('../routes/Room/room.router');
-const post = require('../routes/workspace/post.router');
 const users = require('../routes/admin/user.router');
 
 router.use('/', auth);
