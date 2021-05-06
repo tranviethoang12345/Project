ddad243f32803b318552206990e917a18b031e65
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2021-05-06 16:38:39 +0700

    update user admin 6/5

--- client/src/components/Admin/includes/Navbar/Navbar.js
+++ client/src/components/Admin/includes/Navbar/Navbar.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Container, Typography } from '@material-ui/core';
-import useStyles from './styles'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Container, Typography, AppBar } from "@material-ui/core";
+import useStyles from "./styles";
 
 const Header = () => {
   const classes = useStyles();
   return (
-    <React.Fragment>
-      <header id="header">
-        <nav>
-          <Container>
-            <div class="text-center">
-              <Button class="nav-brand text-dark" component={Link} to="/home">
-                User Management System
-              </Button>
-            </div>
-          </Container>
-        </nav>
-      </header>
-    </React.Fragment>
+    <AppBar className={classes.AdminHome_appBar} position="static">
+      <Container>
+          <nav>
+            <Container>
+              <div class="text-center">
+                <Button class="nav-brand text-dark" component={Link} to="/home">
+                  User Management System
+                </Button>
+              </div>
+            </Container>
+          </nav>
+      </Container>
+    </AppBar>
   );
 };
 
--- client/src/components/Admin/includes/Navbar/styles.js
+++ client/src/components/Admin/includes/Navbar/styles.js
@@ -1,4 +1,14 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
-}))
+  AdminHome_appBar: {
+    margin: "0 0 30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "1em 50px",
+    backgroundColor: "#06d6a0",
+    width: "100%",
+  },
+}));
